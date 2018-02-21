4cca0f9819c71167531aa5fdb0677765c8ddc85c
Dominic Barrett <dominicbarrett@128-122-6-143.DYNAPOOL.NYU.EDU>
2018-02-21 00:01:58 -0500

    keep on working etc

--- TemperatureServer/temperatureServer.js
+++ TemperatureServer/temperatureServer.js
@@ -34,70 +34,77 @@ if(!adc.busy)
     console.log("Pin 0 Data: "+data);
     console.log("Pin 0 Temp Reading: " + tempReading);
    }    // any other data processing code goes here...
+
+
+
+   /*
+   	Simple HTTP/HTTPS request example
+   	Demonstrates a POST request with the body sent as JSON
+   	created 19 April 2015
+   	by Tom Igoe
+   	based on node.js and stackoverflow examples
+
+     //found here:
+     https://github.com/tigoe/NodeExamples/tree/master/SimpleClient
+
+
+   */
+
+   // you can do this with http or https:
+   var http = require('https');
+
+   // make the POST data a JSON object and stringify it:
+   var postData =JSON.stringify({
+     "macAddress":"12:12",
+     "sessionKey": "1212",
+     "data": {"temp":tempReading}
+   });
+
+   /*
+    set up the options for the request.
+    the full URL in this case is:
+    http://example.com:443/login
+   */
+
+   var options = {
+     host: 'connected-devices-itp.herokuapp.com',
+     port: 443,
+     path: '/add',
+   	method: 'POST',
+   	headers: {
+       'Content-Type': 'application/json',
+       'Content-Length': postData.length
+     }
+   };
+
+   /*
+   	the callback function to be run when the response comes in.
+   	this callback assumes a chunked response, with several 'data'
+   	events and one final 'end' response.
+   */
+   function callback(response) {
+     var result = '';		// string to hold the response
+
+     // as each chunk comes in, add it to the result string:
+     response.on('data', function (data) {
+       result += data;
+     });
+
+     // when the final chunk comes in, print it out:
+     response.on('end', function () {
+       console.log(result);
+     });
+   }
+
+   // make the actual request:
+   var request = http.request(options, callback);	// start it
+   request.write(postData);							// send the data
+   request.end();												// end it
+
+
+
+
+   //then...
+   //ALLLL the way back, we're ending the encapsulation of the adc read
   );
 }
-
-
-/*
-	Simple HTTP/HTTPS request example
-	Demonstrates a POST request with the body sent as JSON
-	created 19 April 2015
-	by Tom Igoe
-	based on node.js and stackoverflow examples
-
-  //found here:
-  https://github.com/tigoe/NodeExamples/tree/master/SimpleClient
-
-
-*/
-
-// you can do this with http or https:
-var http = require('https');
-
-// make the POST data a JSON object and stringify it:
-var postData =JSON.stringify({
-  "macAddress":"12:12",
-  "sessionKey": "1212",
-  "data": {"temp":tempReading}
-});
-
-/*
- set up the options for the request.
- the full URL in this case is:
- http://example.com:443/login
-*/
-
-var options = {
-  host: 'connected-devices-itp.herokuapp.com',
-  port: 443,
-  path: '/add',
-	method: 'POST',
-	headers: {
-    'Content-Type': 'application/json',
-    'Content-Length': postData.length
-  }
-};
-
-/*
-	the callback function to be run when the response comes in.
-	this callback assumes a chunked response, with several 'data'
-	events and one final 'end' response.
-*/
-function callback(response) {
-  var result = '';		// string to hold the response
-
-  // as each chunk comes in, add it to the result string:
-  response.on('data', function (data) {
-    result += data;
-  });
-
-  // when the final chunk comes in, print it out:
-  response.on('end', function () {
-    console.log(result);
-  });
-}
-
-// make the actual request:
-var request = http.request(options, callback);	// start it
-request.write(postData);							// send the data
-request.end();												// end it
